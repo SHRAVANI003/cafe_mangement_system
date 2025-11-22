08358d9c80a24a04d93c3e964c761b53673c2894
SHRAVANI003 <85429750+SHRAVANI003@users.noreply.github.com>
2025-11-22 15:57:09 +0530

    Refactor order processing and total calculation
    Refactor cafe management system to initialize total amount and streamline order processing.

--- cafe_management.py
+++ cafe_management.py
@@ -1,25 +1,23 @@
-
-
-# Completed
-#  cafe mgmt system to order food based on your taste.
-
-
 menu = {
     "pizza": 149,
     "burger": 99,
     "coffee": 40,
-    "tea" : 15,
-    "sandwich" : 70,
-    "pasta" : 120,
+    "tea": 15,
+    "sandwich": 70,
+    "pasta": 120,
 }
 
 print("\n\t\t\t---Welcome To Diva Cafe---")
 print("Choose item From the Menu :-")
 user_choice = input("\npizza : 149\nburger : 99\ncoffee : 40\ntea : 15\nsandwich : 70\npasta : 120\n\nChoose :-")
 
+total = 0  # ✅ Initialize total at the start
+
 if user_choice in menu:
     quantity1 = int(input("Enter Quantity: "))
-    print (f"Your order {user_choice} - {menu[user_choice]} will be added")
+    print(f"Your order {user_choice} - {menu[user_choice]} will be added")
+    total += menu[user_choice] * quantity1   # ✅ Add first item to total
+
     print("Want to add another item in list: yes/no")
     choice = input()
 
@@ -28,24 +26,15 @@ if user_choice in menu:
 
         if user_choice2 in menu:
             quantity2 = int(input("Enter Quantity: "))
-            print(f"item {user_choice2} - {menu[user_choice]} will be added")
-            total = (menu[user_choice] * quantity1) + (menu[user_choice2] * quantity2)
-            print(f"Total amount you have to pay : {total}.00")
+            print(f"item {user_choice2} - {menu[user_choice2]} will be added")
+            total += menu[user_choice2] * quantity2   # ✅ Add second item to total
         else:
-            print("Sorry we couldn't find any item related to ",user_choice)
-    else:
-        print("hmmmm...")
-        one_order = menu[user_choice] * quantity1
-        print(f"Total amount you have to pay : {one_order}.00")
+            print("Sorry we couldn't find any item related to", user_choice2)
 
 else:
-    print("Sorry we couldn't find any item related to ",user_choice)
-
-prices = [120, 250, 80]   # sample values
-total = sum(prices)       # define total
+    print("Sorry we couldn't find any item related to", user_choice)
 
 print("\n--- Your Bill ---")
-print(f"Total Amount: ₹{total}")
+print(f"Total Amount: ₹{total}.00")
 print("Thanks for visiting Diva Cafe!")
-
 print("Have a Beautiful Day... ")
